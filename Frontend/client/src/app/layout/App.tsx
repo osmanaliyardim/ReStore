8f7c1ff19a2140fade9fae5213b899a71a597ef8
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-07-21 13:44:15 +0300

    Adding Theme configurations

--- Frontend/client/src/app/layout/App.tsx
+++ Frontend/client/src/app/layout/App.tsx
@@ -1,16 +1,32 @@
 import Catalog from "../../features/catalog/Catalog";
-import { Container, CssBaseline } from "@mui/material";
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Header from "./Header";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  const handleThemeChange = () => {
+    setDarkMode(!darkMode);
+  }
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline/>
-      <Header/>
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
         <Catalog/>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
