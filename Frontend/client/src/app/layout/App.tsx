8a5a88d2209c779ac1b6bdde1b3d23a5c89a4df1
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-07-24 09:13:51 +0300

    Creating basket slice

--- Frontend/client/src/app/layout/App.tsx
+++ Frontend/client/src/app/layout/App.tsx
@@ -4,25 +4,26 @@ import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import { useStoreContext } from "../context/StoreContext";
 import Utils from "../util/Utils";
 import agent from "../api/agent";
 import Loading from "./Loading";
 import Constants from "../constants/Constants";
+import { useAppDispatch } from "../store/configureStore";
+import { setBasket } from "../../features/basket/basketSlice";
 
 function App() {
-  const {setBasket} = useStoreContext();
+  const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const buyerId = Utils.getCookie('buyerId');
     if(buyerId){
       agent.Basket.get()
-        .then(basket => setBasket(basket))
+        .then(basket => dispatch(setBasket(basket)))
         .catch(error => console.error(error))
         .finally(() => setLoading(false));
     } else setLoading(false);
-  }, [setBasket]);
+  }, [dispatch]);
 
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
