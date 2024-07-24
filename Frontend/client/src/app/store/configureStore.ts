f928c0f25410d81e5b2884cd541ca3f6e4b28712
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-07-24 08:34:44 +0300

    Using Redux Toolkit

--- Frontend/client/src/app/store/configureStore.ts
+++ Frontend/client/src/app/store/configureStore.ts
@@ -1,8 +1,15 @@
-import { createStore } from "redux"
-import counterReducer from "../../features/contact/counterReducer"
+import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "../../features/contact/counterSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
-const configureStore = () => {
-    return createStore(counterReducer)
-}
+export const store = configureStore({
+    reducer: {
+        counter: counterSlice.reducer
+    }
+});
 
-export default configureStore;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- Frontend/client/src/index.tsx
+++ Frontend/client/src/index.tsx
@@ -8,10 +8,8 @@ import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
 import { StoreProvider } from './app/context/StoreContext';
-import configureStore from './app/store/configureStore';
 import { Provider } from 'react-redux';
-
-const store = configureStore();
+import { store } from './app/store/configureStore';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
