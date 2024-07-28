3c9f6a8246821f3b769189c58302f3db72dc1fc3
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-07-28 11:13:27 +0300

    Using React Hook Form validators

--- Frontend/client/src/features/account/Login.tsx
+++ Frontend/client/src/features/account/Login.tsx
@@ -12,16 +12,22 @@ import { FieldValues, useForm } from 'react-hook-form';
 import { LoadingButton } from '@mui/lab';
 
 const Login = () => {
-  const {register, handleSubmit, formState: {isSubmitting}} = useForm()
+  const {register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm({
+    mode: "onTouched"
+  });
 
   const submitForm = async (data: FieldValues) => {
-    await agent.Account.login(data);
+    try {
+      await agent.Account.login(data);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
       <Container component={Paper} maxWidth="sm" sx={{display: "flex", flexDirection: "column", alignItems: "center", p: 4}}>
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
+          <Avatar sx={{ m: 1, bgcolor: 'green' }}>
+            <LockOutlinedIcon/>
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
@@ -32,21 +38,27 @@ const Login = () => {
               fullWidth
               label="Username"
               autoFocus
-              {...register('username')}
+              {...register('username', {required: "*Username is required"})}
+              error={!!errors.username}
+              helperText={errors?.username?.message as string}
             />
             <TextField
               margin="normal"
               fullWidth
               label="Password"
               type="password"
-              {...register('password')}
+              {...register('password', {required: "*Password is required"})}
+              error={!!errors.password}
+              helperText={errors?.password?.message as string}
             />
             <LoadingButton
               loading={isSubmitting}
+              disabled={!isValid}
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 4 }}
+              color="success"
+              sx={{ mt: 3, mb: 4, height: 50 }}
             >
               Sign In
             </LoadingButton>
