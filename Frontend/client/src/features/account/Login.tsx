2127fbabc401e4573a749fbd065bb11e29242bf8
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-07-28 15:34:06 +0300

    Adding private route component

--- Frontend/client/src/features/account/Login.tsx
+++ Frontend/client/src/features/account/Login.tsx
@@ -6,7 +6,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Divider, Paper } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { FieldValues, useForm } from 'react-hook-form';
 import { LoadingButton } from '@mui/lab';
 import { useAppDispatch } from '../../app/store/configureStore';
@@ -14,6 +14,7 @@ import { signInUser } from './accountSlice';
 
 const Login = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const dispatch = useAppDispatch();
   const {register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm({
     mode: "onTouched"
@@ -22,7 +23,7 @@ const Login = () => {
   const submitForm = async (data: FieldValues) => {
     try {
       await dispatch(signInUser(data));
-      navigate('/catalog');
+      navigate(location.state?.from || '/catalog');
     } catch (error: any) {
       console.error(error);
     }
