f18f274060e2e68e6463de5295f1f6873d70195e
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-07-31 11:07:12 +0300

    Adding cloudinary image service

--- Backend/ReStoreWebAPI/Program.cs
+++ Backend/ReStoreWebAPI/Program.cs
@@ -82,8 +82,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddAuthorization();
 
 builder.Services.AddScoped<TokenService>();
-
 builder.Services.AddScoped<PaymentService>();
+builder.Services.AddScoped<ImageService>();
 
 var app = builder.Build();
 
